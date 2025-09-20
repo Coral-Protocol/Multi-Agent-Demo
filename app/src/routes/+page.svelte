6d20905b350467661f46654e5964f27525c30d37
Alan Panayotov <alanpanayotov@gmail.com>
2025-09-20 02:40:33 +0100

    fix: loading state

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -4,18 +4,22 @@
 
   let results: string[] = $state([])
   let query = $state('');
+
+  let loading = $state(false);
 </script>
 <main class="flex flex-col items-center justify-center pt-64">
   <div class="flex flex-row gap-2 w-[70%] mb-5">
   <input class="input" type="text" placeholder="Input" bind:value={query}/>
     <button type="button" class="btn-icon preset-filled" title="Go" aria-label="Go" onclick={async() => {
       results = [];
+      loading = true;
       const res = await fetch(encodeURI(`/search?q=${query}`));
       const data = await res.json();
       results.push(data.result);
+      loading = false;
     }}><Search size={18} /></button>
   </div>
-  {#if results.length == 0}
+  {#if loading}
     <LoaderCircle class="animate-spin mt-20" size={30} />
   {/if}
   <div class="w-[60ch]">
